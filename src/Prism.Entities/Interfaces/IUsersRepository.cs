803b50d5d5de3d22c0f3ed72e728d3ad00ed698b
Daniel Plawgo <daniel.plawgo@gmail.com>
2013-05-13 18:16:29 +0200

    zmiana nazwy interfejsu.

--- src/Prism.Entities/Interfaces/IUsersRepository.cs
+++ src/Prism.Entities/Interfaces/IUsersRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Entities.Users;
 
 namespace Prism.Entities.Interfaces
 {
-    interface IUserRepository
+    interface IUsersRepository
     {
+        IEnumerable<User> GetUsers(int index, int count);
     }
 }
